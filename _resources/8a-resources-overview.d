f84dda0292afda3feb1f7018e3696618a94577fe
chuasweechin <chua_Swee_chin@data.gov.sg>
2018-10-27 01:14:54 +0800

    Create 8a-resources-overview.d

---
+++ _resources/8a-resources-overview.d
@@ -0,0 +1,14 @@
+---
+layout: leftnav-page-content
+title: Overview
+permalink: /resources/overview/
+breadcrumb: Resources - Overview
+collection_name: resources
+second_nav_title: "Resources"
+---
+#### **Resources**
+![Resources](/images/resources/resources.PNG)
+
+For resources, there will be 12 articles that will be displayed for each page. 
+You may define the category name of the resources page. There is also a filter for dates.
+
